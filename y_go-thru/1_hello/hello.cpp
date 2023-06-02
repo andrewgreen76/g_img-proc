19970dc42393a096507c96f402f8671b499187ad
Andrew Green <andrewgreen031991@gmail.com>
2023-06-02 09:16:47 -0400

    x

--- y_go-thru/1_hello/hello.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main()
-{
-  cout << "\nHello, world!\n\n";
-  
-  return 0;
-}
--- y_go-thru/z.sh
+++
@@ -1,2 +0,0 @@
-child_dirs=$(find / -maxdepth 1 -type d | tail -n +2 | head -n 3)
-echo "$child_dirs"
